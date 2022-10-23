5adb4affefe4c18638db689c4b45fde4764ca6b9
pablojr17 <pablojr17@gmail.com>
2022-10-23 16:24:45 -0300

    Add Regex nome

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ function setCardType(type) {
     visa: ["#436d99", "#2d57f2"],
     elo: ["#EF4123", "#FFCB05"],
     mastercard: ["#df6f29", "#c69347"],
+    diners: ["#0079BE", "#ffffff"],
     default: ["black", "gray"],
   }
   ccBgColor01.setAttribute("fill", colors[type][0])
@@ -17,6 +18,7 @@ function setCardType(type) {
   ccLogo.setAttribute("src", `cc-${type}.svg`)
   ccColorButton.backgroundColor = colors[type][0]
 }
+globalThis.setCardType = setCardType
 
 const securityCode = document.querySelector("#security-code")
 const securityCodePattern = { mask: "0000" }
@@ -55,6 +57,17 @@ const cardNumberPattern = {
     },
     {
       mask: "0000 0000 0000 0000",
+      regex: /(36[0-8][0-9]{3}|369[0-8][0-9]{2}|3699[0-8][0-9]|36999[0-9])/,
+      cardType: "diners",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex:
+        /^4011(78|79)|^43(1274|8935)|^45(1416|7393|763(1|2))|^50(4175|6699|67[0-6][0-9]|677[0-8]|9[0-8][0-9]{2}|99[0-8][0-9]|999[0-9])|^627780|^63(6297|6368|6369)|^65(0(0(3([1-3]|[5-9])|4([0-9])|5[0-1])|4(0[5-9]|[1-3][0-9]|8[5-9]|9[0-9])|5([0-2][0-9]|3[0-8]|4[1-9]|[5-8][0-9]|9[0-8])|7(0[0-9]|1[0-8]|2[0-7])|9(0[1-9]|[1-6][0-9]|7[0-8]))|16(5[2-9]|[6-7][0-9])|50(0[0-9]|1[0-9]|2[1-9]|[3-4][0-9]|5[0-8]))/,
+      cardType: "elo",
+    },
+    {
+      mask: "0000 0000 0000 0000",
       cardType: "default",
     },
   ],
@@ -69,4 +82,47 @@ const cardNumberPattern = {
 
 const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
 
-globalThis.setCardType = setCardType
+const addButton = document.querySelector("#add-card")
+addButton.addEventListener("click", () => {
+  alert("cartão adicionado")
+})
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault()
+})
+
+const cardHolder = document.querySelector("#card-holder")
+cardHolder.addEventListener("input", () => {
+  const ccHolder = document.querySelector(".cc-holder .value")
+  ccHolder.innerText =
+    cardHolder.value.length === 0 ? "FULANO DA SILVA" : cardHolder.value
+})
+
+securityCodeMasked.on("accept", () => {
+  updateSecurityCode(securityCodeMasked.value)
+})
+
+function updateSecurityCode(code) {
+  const ccSecurity = document.querySelector(".cc-security .value")
+  ccSecurity.innerText = code.length === 0 ? "123" : code
+}
+
+cardNumberMasked.on("accept", () => {
+  const cardType = cardNumberMasked.masked.currentMask.cardType
+  setCardType(cardType)
+  updateCardNumber(cardNumberMasked.value)
+})
+
+function updateCardNumber(number) {
+  const ccNumber = document.querySelector(".cc-number")
+  ccNumber.innerText = number.length === 0 ? "1234 5678 9012 3456" : number
+}
+
+expirationDateMasked.on("accept", () => {
+  updateExpirationDate(expirationDateMasked.value)
+})
+
+function updateExpirationDate(date) {
+  const ccExpiration = document.querySelector(".cc-extra .cc-expiration .value")
+  ccExpiration.innerText = date.length === 0 ? "02/32" : date
+}
