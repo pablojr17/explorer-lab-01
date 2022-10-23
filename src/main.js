5e236d14fcd3414cdd10b9232c377af25d92a09c
pablojr17 <pablojr17@gmail.com>
2022-10-23 16:26:42 -0300

    Remove condition button

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import IMask from "imask"
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
 const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
-const ccColorButton = document.querySelector("form button").style
+// const ccColorButton = document.querySelector("form button").style
 
 function setCardType(type) {
   const colors = {
@@ -16,7 +16,6 @@ function setCardType(type) {
   ccBgColor01.setAttribute("fill", colors[type][0])
   ccBgColor02.setAttribute("fill", colors[type][1])
   ccLogo.setAttribute("src", `cc-${type}.svg`)
-  ccColorButton.backgroundColor = colors[type][0]
 }
 globalThis.setCardType = setCardType
 
